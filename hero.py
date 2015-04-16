8dd01e3c63fab31f9dd74873fc73d547e21eccf2
Gabriela Grozeva <gg_grozeva@mail.bg>
2015-04-16 21:25:20 +0300

    next

--- hero.py
+++ hero.py
@@ -3,8 +3,10 @@ class Hero:
     def __init__ (self, name, title, health, mana, mana_regeneration_rate):
         self.name = name
         self.title = title
-        self.health = 100
-        self.mana = 100
+        self.start_health=100
+        self.health = health
+        self.start_mana=100
+        self.mana = mana
         self.mana_regeneration_rate = 2
 
     def known_as(self):
@@ -21,6 +23,13 @@ class Hero:
 
     def get_mana (self):
         return self.mana
+
+    def is_alive(self):
+        if get_heath(self)>0:
+            return True
+        return False
+
+
 
 
 
