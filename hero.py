d758d17920eb489506d7888a918ab7ca9a83477c
Gabriela Grozeva <gg_grozeva@mail.bg>
2015-04-16 22:41:11 +0300

    first day end

--- hero.py
+++ hero.py
@@ -9,51 +9,79 @@ class Hero:
         self.__mana = mana
         self.__mana_regeneration_rate = 2
 
+
     def known_as( self):
+
         return "{} the {}".format(self.get_name(), self.get_title())
 
+
     def get_name( self):
+
         return self.__name
 
+
     def get_title ( self):
+
         return self.__title
 
+
     def get_health( self):
+
         return self.__health
 
+
     def get_mana ( self):
+
         return self.__mana
 
+
     def is_alive( self):
+
         if get_health( self) > 0:
             return True
         return False
 
+
     def can_cast( self):
+
         if get_mana( self) > 0:
             return True
         return False
 
+
     def take_damage( self, damage_points):
+
         if self.__health < damage_points:
             self.__health = 0
         else:
             self.__health = self.__health - damage_points
 
+
     def take_healing( self, healing_points):
+        if is_alive(self) == False
+            return False
+
         a = self.__health + healing_points
+
         if a > self.__start_health:
             self.__health = self.__start_health
         else:
             self.__health = a
 
+        return True
+
+
     def take_mana( self, mana_points):
         a = self.__mana + mana_points
+
         if a > self.__start_mana:
             self.__mana = self.__start_mana
         else:
             self.__mana = a
 
+
+    def equip(self, weapon):
+        pass
 
 
 
