abcc42ab29ea6320d217a36aa772b5fde535de4e
Gabriela Grozeva <gg_grozeva@mail.bg>
2015-04-17 13:55:21 +0300

    two classes- weapon and spell

--- weapon_and_spell.py
+++ weapon_and_spell.py
@@ -1,5 +1,28 @@
 
 class Weapon:
-    def __init__(self, name, damage):
+
+    def __init__( self, name, damage):
+        self.__name = name
+        self.__damage = 20
+
+    def get_damage( self):
+        return self.__damage
 
 
+
+class Spell:
+
+    def __init__( self, name, damage, mana_cost, cast_range):
+        self.__name = name
+        self.__damage = 20
+        self.__mana_cost = 30
+        self.__cast_range = 1
+
+    def get_damage( self):
+        return self.__damage
+
+    def get_mana_cost( self):
+        return self.__mana_cost
+
+    def get_cast_range( self):
+        return self.__cast_range
